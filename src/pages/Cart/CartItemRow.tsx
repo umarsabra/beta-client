0324a70844c4bc79cb10e88053efe58884002fb2
umarsabra <omaarsaabra@outlook.com>
2022-11-07 21:06:29 +0200

    added btn and quantity handler

--- src/pages/Cart/CartItemRow.tsx
+++ src/pages/Cart/CartItemRow.tsx
@@ -6,7 +6,10 @@ function CartItemRow(order_item: OrderItem) {
   return (
     <div className="cart-item-row table-row">
       <div className="title-cell">{order_item.title}</div>
-      <div className="quantity-cell number-cell">{order_item.quantity}</div>
+      <div className="quantity-cell number-cell">
+        <input type="number" min={1} defaultValue={order_item.quantity} />
+        <button className="remove-btn">حذف</button>
+      </div>
       <div className="price-cell number-cell">{order_item.price}</div>
       <div className="total-cost-cell number-cell">
         {order_item.total_price}
