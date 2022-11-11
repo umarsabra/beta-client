83e7b0e87c07722ba4ee6e8e9c47136fabd4cd48
umarsabra <omaarsaabra@outlook.com>
2022-11-11 03:30:34 +0200

    changed api url

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 import Menu from "./components/Menu";
 function App() {
 
-  localStorage.setItem("API_URL", "http://192.168.1.108:8080/api/v1")
+  localStorage.setItem("API_URL", "http://192.168.71.64:8080/api/v1")
   return (
     <div className="App">
       <Menu />
